cdc67a10bf3c781f08b76c74ee1af0c4c05ce904
FelipeLopes1996 <felipel.lima1996@gmail.com>
2024-06-24 19:17:24 -0300

    test urls

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,12 @@ import Header from './shared/components/Header';
 import { Box } from '@mui/material';
 import SideBarLinks from './shared/components/SideBar';
 
+const API_URL = import.meta.env.VITE_APP_API_URL;
+const APP_URL = import.meta.env.VITE_APP_URL;
+
+console.log(API_URL);
+console.log(APP_URL);
+
 const App = () => {
   return (
     <Box
